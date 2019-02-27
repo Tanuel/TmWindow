99800fe67a898190844ecaafc6d9b2ed6d6edbf3
Tanuel <tanuel.mategi@gmail.com>
2019-02-27 22:59:03 +0100

    Update example

--- src/docs/js/example.js
+++ src/docs/js/example.js
@@ -7,9 +7,17 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('apply-window-title').addEventListener('click', function(){
        tmw.title = document.getElementById('set-window-title').value;
     });
+
+    document.getElementById('set-window-title').addEventListener('keyup', function(){
+       tmw.title = document.getElementById('set-window-title').value;
+    });
+
     document.getElementById('apply-window-content').addEventListener('click', function(){
        tmw.content = document.getElementById('set-window-content').value;
     });
 
+    document.getElementById('set-window-content').addEventListener('keyup', function(){
+       tmw.content = document.getElementById('set-window-content').value;
+    });
     window.tmw = tmw;
 });
